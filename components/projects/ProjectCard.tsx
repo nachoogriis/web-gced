8657c7cde8693bb20b73c637e090da26b4685176
nachoogriis <nachogrismartin@gmail.com>
2025-01-21 13:10:24 +0100

    Updates in ProjectCard

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -14,19 +14,17 @@ export default function({
     project: ProjectInfo
 }) {
     return (
-        <div className="relative flex flex-row items-center w-[100%] bg-white group rounded-[10px] border border-[#B0DAED]">
-            <div className="flex flex-row items-center mr-2 rounded-tl-[15px] rounded-bl-[10px]">
-                <img src={project.imagePath} alt={project.topic} className="h-[200px] w-auto rounded-tl-[10px] rounded-bl-[10px]" />
-                <div className="ml-4">
+        <div className="relative flex flex-row items-center w-[100%] bg-white group rounded-[10px] border border-[#B0DAED] gap-4">
+            <img src={project.imagePath} alt={project.topic} className="h-[200px] w-auto rounded-tl-[10px] rounded-bl-[10px]" />
+            <div className="flex flex-col items-center justify-center gap-5 mr-4">
+                <div className="text-center">
                     <h2 className="text-xl font-bold">{project.name}</h2>
                     <p className="text-gray-700">{project.description}</p>
                 </div>
-            </div>
-            <div className="absolute inset-0 p-6 bg-white opacity-0 group-hover:opacity-60 flex items-end justify-center rounded-[10px]">
-                <GcedButton>
+                <GcedButton width = "200px">
                     <div className="flex flex-row items-center justify-center gap-2">
-                        <InfoIcon />
-                        <h1>Més informació</h1>
+                    <InfoIcon />
+                    <h1>Més informació</h1>
                     </div>
                 </GcedButton>
             </div>
