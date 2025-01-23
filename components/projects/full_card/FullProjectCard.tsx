521305ad47910d275bfc28fb7521ff94d20169f6
nachoogriis <nachogrismartin@gmail.com>
2025-01-23 16:54:34 +0100

    Update Full Project Card

--- components/projects/full_card/FullProjectCard.tsx
+++ components/projects/full_card/FullProjectCard.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import ProjectTitle from "./ProjectTitle";
+import ImageGallery from "./ImageGallery";
 
 interface ProjectInfo {
   imagePath: string;
@@ -15,11 +18,9 @@ export default function FullProjectCard({
   return (
     <div className="flex items-center justify-center max-h-none">
       <div className="flex flex-row gap-10 items-center justify center w-[95%] h-[95%]">
-        <div className="flex flex-col w-[50%] h-[100%]">
+        <div className="flex flex-col w-[50%] h-[100%] gap-10">
           <ProjectTitle title = {project.name}/>
-          <div className="flex items-center justify-center h-[80%]">
-            <img src={project.imagePath} alt={project.topic} className="h-auto" />
-          </div>
+          <ImageGallery />
         </div>
         <div className="border-2 border-red-700 w-[50%] h-[100%]">
           
