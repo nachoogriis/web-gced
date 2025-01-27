efd04f40b85a68fc2635537be726a605a95567a5
mariarisquesmontalban <mariarisques@gmail.com>
2025-01-27 11:00:05 +0100

    Remove debug code

--- components/alumni_card/FullAlumniCard.tsx
+++ components/alumni_card/FullAlumniCard.tsx
@@ -8,6 +8,7 @@ import {
 import { AlumniCardInfo } from "@/lib/db/alumni"
 import GcedButton from "../GcedButton"
 import { InfoIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 type Props = {
   alumni: AlumniCardInfo
@@ -16,12 +17,22 @@ export default function FullAlumniCard({ alumni }: Props) {
   return (
     <Dialog>
       <DialogTrigger>
-        <GcedButton>
-          <InfoIcon className="text-lg" />
-          Més informació
-        </GcedButton>
+        <div
+          className={cn(
+            "inline-flex items-center rounded-[10px] border-[1px]",
+            "border-[#009DE4] bg-white-600 px-3 py-2 text-sm font-semibold",
+            "text-[#009DE4] hover:text-white hover:bg-[#009DE4]",
+            "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+            "focus-visible:outline-[#009DE4]"
+          )}
+        >
+          <div className="flex flex-row items-center justify-center gap-2">
+            <InfoIcon />
+            <h1>Més informació</h1>
+          </div>
+        </div>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="w-[40%] h-[90%] max-w-none">
         <DialogHeader>
           <DialogTitle>
             {alumni.firstName} {alumni.lastName}
