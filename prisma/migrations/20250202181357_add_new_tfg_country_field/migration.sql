d6775ecf30368eb3622b14b7f58bb85a91182cf7
nachoogriis <nachogrismartin@gmail.com>
2025-02-02 19:14:33 +0100

    Add TFG Country field to database

---
+++ prisma/migrations/20250202181357_add_new_tfg_country_field/migration.sql
@@ -0,0 +1,21 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Alumni" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "linkedInURL" TEXT,
+    "generation" INTEGER NOT NULL,
+    "review" TEXT,
+    "tfgTitle" TEXT NOT NULL,
+    "tfgDescription" TEXT NOT NULL,
+    "tfgUniversity" TEXT NOT NULL DEFAULT 'Universitat Politècnica de Catalunya',
+    "tfgCountry" TEXT NOT NULL DEFAULT 'Spain',
+    "currentJob" TEXT
+);
+INSERT INTO "new_Alumni" ("currentJob", "firstName", "generation", "id", "lastName", "linkedInURL", "review", "tfgDescription", "tfgTitle", "tfgUniversity") SELECT "currentJob", "firstName", "generation", "id", "lastName", "linkedInURL", "review", "tfgDescription", "tfgTitle", "tfgUniversity" FROM "Alumni";
+DROP TABLE "Alumni";
+ALTER TABLE "new_Alumni" RENAME TO "Alumni";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
