321e7fc6f91f69fd5161da5892444f5a24eac833
nachoogriis <nachogrismartin@gmail.com>
2025-02-03 10:46:29 +0100

    Refactor Full Alumni Card to components

--- components/alumni_card/full_card/FullAlumniCard.tsx
+++ components/alumni_card/full_card/FullAlumniCard.tsx
@@ -1,6 +1,11 @@
 import { DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { AlumniCardInfo } from "@/lib/db/alumni";
 import AlumniTopPart from "@/components/alumni_card/AlumniTopPart";
+import InternshipSection from "./InternshipSection";
+import TfgSection from "./TfgSection";
+import MasterSection from "./MasterSection";
+import CurrentJobSection from "./CurrentJobSection";
+import ProjectsSection from "./ProjectsSection";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import Image from "next/image"
 
@@ -9,6 +14,20 @@ type Props = {
 };
 
 export default function FullAlumniCard({ alumni }: Props) {
+  const {
+    firstName,
+    lastName,
+    generation,
+    linkedInURL,
+    internships,
+    tfgTitle, 
+    tfgDescription,
+    tfgCountry,
+    tfgUniversity,
+    masters,
+    currentJob,
+    projects
+   } = alumni
   return (
     <>
       <DialogHeader>
@@ -16,17 +35,17 @@ export default function FullAlumniCard({ alumni }: Props) {
           <DialogTitle>Informació Completa</DialogTitle>
         </VisuallyHidden>
 
-        <div className="flex flex-row items-center justify-center transform scale-[1.3] pr-[20px]">
+        <div className="flex flex-row items-center justify-center transform scale-[1.3] pr-[20px] pb-[10px]">
           <AlumniTopPart
-            name={alumni.firstName}
-            surname={alumni.lastName}
-            generation={alumni.generation}
+            name={firstName}
+            surname={lastName}
+            generation={generation}
           />
 
-          {alumni.linkedInURL !== "No especificat" && (
+          {linkedInURL !== "No especificat" && (
             <button
             className="pt-[20px]"
-            onClick={() => window.open(alumni.linkedInURL, "_blank")}
+            onClick={() => window.open(linkedInURL, "_blank")}
           >
             <Image src="linkedin-icon.svg" alt="Open Link" width={40} height={40} />
           </button>
@@ -36,107 +55,39 @@ export default function FullAlumniCard({ alumni }: Props) {
 
       <div className="gap-4 overflow-y-auto">
         {/* Prácticas en empresa */}
-        {alumni.internships.length > 0 && (
-          <section>
-            <h2 className="text-m font-semibold border-b pb-2 mt-6 mb-2">
-              Pràctiques a empresa
-            </h2>
-            {alumni.internships.map((internship, index) => (
-              <div key={index} className="mb-4">
-                <p className="text-sm">
-                  <span className="font-bold">Empresa:</span> {internship.organization}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">País:</span> {internship.country}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">Tema:</span> {internship.position}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">Descripció:</span> {internship.description}
-                </p>
-              </div>
-            ))}
-          </section>
+        {internships.length > 0 && (
+          <InternshipSection
+            internships={internships}
+          />
         )}
 
         {/* Trabajo de final de grado */}
-        <section>
-          <h2 className="text-m font-semibold border-b pb-2 mt-6 mb-2">
-            Treball de final de grau
-          </h2>
-          <div className="mb-4">
-            <p className="text-sm">
-              <span className="font-bold">Títol:</span> {alumni.tfgTitle || "No especificat"}
-            </p>
-            <p className="text-sm">
-              <span className="font-bold">Descripció:</span> {alumni.tfgDescription || "No especificat"}
-            </p>
-            <p className="text-sm">
-              <span className="font-bold">Universitat:</span> {alumni.tfgUniversity || "No especificat"}
-            </p>
-            <p className="text-sm">
-              <span className="font-bold">País:</span> {alumni.tfgCountry || "No especificat"}
-            </p>
-          </div>
-        </section>
+        <TfgSection 
+          tfgTitle={tfgTitle}
+          tfgDescription={tfgDescription}
+          tfgUniversity={tfgUniversity}
+          tfgCountry={tfgCountry}
+        />
 
         {/* Máster */}
-        {alumni.masters.length > 0 && (
-          <section>
-            <h2 className="text-m font-semibold border-b pb-2 mt-6 mb-2">
-              Màster
-            </h2>
-            {alumni.masters.map((master, index) => (
-              <div key={index} className="mb-4">
-                <p className="text-sm">
-                  <span className="font-bold">Títol del màster:</span> {master.name}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">País:</span> {master.country}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">Universitats:</span> {master.universities}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">Descripció:</span> {master.description}
-                </p>
-              </div>
-            ))}
-          </section>
+        {masters.length > 0 && (
+          <MasterSection
+            masters={masters}
+          />
         )}
 
         {/* Trabajo actual */}
-        {alumni.currentJob && (
-          <section>
-            <h2 className="text-m font-semibold border-b pb-2 mt-6 mb-2">
-              Feina Actual
-            </h2>
-            <div className="mb-4">
-              <p className="text-sm">
-                <span className="font-bold">Lloc de treball:</span> {alumni.currentJob}
-              </p>
-            </div>
-          </section>
+        {currentJob && (
+          <CurrentJobSection
+            currentJob={currentJob}
+          />
         )}
 
         {/* Proyectos personales */}
-        {alumni.projects.length > 0 && (
-          <section>
-            <h2 className="text-m font-semibold border-b pb-2 mt-6 mb-2">
-              Projectes Personals
-            </h2>
-            {alumni.projects.map((project, index) => (
-              <div key={index} className="mb-4">
-                <p className="text-sm">
-                  <span className="font-bold">Nom:</span> {project.name}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">Descripció:</span> {project.description}
-                </p>
-              </div>
-            ))}
-          </section>
+        {projects.length > 0 && (
+          <ProjectsSection
+            projects={projects}
+          />
         )}
       </div>
     </>
