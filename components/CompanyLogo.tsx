863f69a271c2d6439b91c3624c046cbe258b9808
mariarisquesmontalban <mariarisques@gmail.com>
2025-08-07 17:26:52 +0200

    Change logos size

--- components/CompanyLogo.tsx
+++ components/CompanyLogo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function CompanyLogo({ image }: { image: string}) {
   return (
-    <div className="border rounded overflow-clip w-10">
+    <div className="border rounded overflow-clip w-20">
       <Image src={image} alt="Google" width={140} height={80} />
     </div>
   );
