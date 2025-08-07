d580f84243024ced9822c76a23b6ad2d80b6eb10
mariarisquesmontalban <mariarisques@gmail.com>
2025-08-07 18:04:36 +0200

    Change project cards responsiveness

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -16,15 +16,15 @@ export default function ProjectCard({ project }: { project: ProjectInfo }) {
   const image_path = `/projects/${image_paths[0]}`;
 
   return (
-    <div className="relative flex flex-row items-center w-[100%] bg-white group rounded-[10px] border border-[#B0DAED] gap-4">
+    <div className="relative flex flex-row items-center w-[100%] bg-white group rounded-[10px] border border-[#B0DAED] gap-4 p-4">
       <Image
         width={651}
         height={465}
         src={image_path}
         alt={project.topic}
-        className="h-[200px] w-auto rounded-tl-[10px] rounded-bl-[10px]"
+        className="hidden md:block h-[200px] w-auto rounded-tl-[10px] rounded-bl-[10px]"
       />
-      <div className="flex flex-col items-center justify-center gap-5 mr-4">
+      <div className="flex flex-col items-center justify-center gap-5 mr-4 line-clamp-4">
         <div className="text-center">
           <h2 className="text-xl font-bold">{project.name}</h2>
           <p className="text-gray-700 line-clamp-3">{project.summary}</p>
