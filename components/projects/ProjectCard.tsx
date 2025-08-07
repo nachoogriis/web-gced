3aa9b87cabfc9972399e75c1ccaf74da6f4598eb
mariarisquesmontalban <mariarisques@gmail.com>
2025-08-07 17:50:19 +0200

    Update project taps list responsiveness

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -27,7 +27,7 @@ export default function ProjectCard({ project }: { project: ProjectInfo }) {
       <div className="flex flex-col items-center justify-center gap-5 mr-4">
         <div className="text-center">
           <h2 className="text-xl font-bold">{project.name}</h2>
-          <p className="text-gray-700">{project.summary}</p>
+          <p className="text-gray-700 line-clamp-3">{project.summary}</p>
         </div>
         <FullProjectDialog project={project} />
       </div>
