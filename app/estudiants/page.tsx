b874406b92693943a276fea27673f24090dbd4c8
Pau Fernández <pau.fernandez@upc.edu>
2025-08-11 17:02:36 +0200

    Activate 'force-dyanmic' for /estudiants

--- app/estudiants/page.tsx
+++ app/estudiants/page.tsx
@@ -1,6 +1,8 @@
 import AlumniPage from "../../components/AlumniPage"
 import { dbAlumniGetAllCardsInfo } from "@/lib/db/alumni"
 
+export const dynamic = "force-dynamic"
+
 export default async function Home() {
   const alumniData = await dbAlumniGetAllCardsInfo()
 
