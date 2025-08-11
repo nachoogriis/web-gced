d405262953f18f673bc2770fde506570421ace21
Pau Fernández <pau.fernandez@upc.edu>
2025-08-11 16:37:29 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 HOST=web-gced
-ssh $HOST "cd web-gced; git pull; docker compose restart; docker image prune -f"
+ssh $HOST <<EOF
+cd web-gced
+git pull
+docker compose down -v
+docker image rm web-gced
+docker image rm web-gced-web-gced
+docker compose up -d
+docker image prune -f
+EOF
