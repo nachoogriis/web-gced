e35368a68528aec172baa69bce552fac61e9c3a6
Pau Fernández <pau.fernandez@upc.edu>
2025-08-11 16:28:56 +0200

    Actualitzo deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-
 HOST=web-gced
-rsync -avz --delete --exclude=node_modules ./ $HOST:web-gced/
-ssh $HOST "cd web-gced; docker compose restart; docker image prune -f"
+ssh $HOST "cd web-gced; git pull; docker compose restart; docker image prune -f"
