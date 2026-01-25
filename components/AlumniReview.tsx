53c763c1cfa079a1de49623486330ada80adcbd3
Maria Risques <mariarisques@gmail.com>
2026-01-25 21:03:05 +0100

    Change format top part reviews

--- components/AlumniReview.tsx
+++ components/AlumniReview.tsx
@@ -14,13 +14,13 @@ export default function AlumniReview({ firstName, lastName, generation, review, 
   return (
     <div className={cn(
     "rounded-tl-[15px] rounded-tr-[15px] rounded-br-[15px] bg-upc-muted overflow-hidden h-[235px] shadow-xl shadow-gray-300",
-    "flex flex-col items-stretch gap-[0.4em]",
+    "flex flex-col items-stretch gap-[0.1em] pt-2",
       )}>
       <div className="flex items-start">
         <AlumniTopPart name={firstName} surname={lastName} generation={generation} id={id} />
       </div>
 
-      <div className="flex mt-4 gap-[15px] px-3 ">
+      <div className="flex gap-[1px] px-3 ">
         <p className="text-[12px] text-gray-600 font-normal leading-normal line-clamp-6">{review}</p>
       </div>
     </div>
--- components/alumni_card/AlumniTopPart.tsx
+++ components/alumni_card/AlumniTopPart.tsx
@@ -11,16 +11,18 @@ interface AlumniTopPartProps {
  
 export default function AlumniTopPart({ name, surname, generation, id = 0 }: AlumniTopPartProps) {
   return (
-    <div className="flex flex-row w-full items-center gap-4 p-3 pb-1">
+    <div className="flex flex-row w-full items-center p-3">
       <div className="w-[75px] h-[75px] flex-shrink-0 rounded-full bg-[#B0DAED]">
         <PersonIcon name={name} surname={surname} />
       </div>
 
-      <div className="flex flex-col justify-center items-start gap-0">
-        <p className="text-black text-xl font-bold leading-none">
-          {name} {surname}
-        </p>
-          <GenerationBadge year={generation} />
+      <div className="flex flex-col justify-center items-start gap-2 p-4">
+        <div className="flex flex-row items-center gap-2">
+          <p className="text-black text-xl font-bold leading-none">
+            {name} {surname}
+          </p>
+        </div>
+        <GenerationBadge year={generation} />
       </div>
     </div>
   )
