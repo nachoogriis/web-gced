ceecb6c51db65cf4f0e0fa845d776020c647086e
Maria Risques <mariarisques@gmail.com>
2026-01-28 19:52:03 +0100

    Add quotes in review

--- components/AlumniReview.tsx
+++ components/AlumniReview.tsx
@@ -1,5 +1,7 @@
 import AlumniTopPart from "@/components/alumni_card/AlumniTopPart"
 import { cn } from "@/lib/utils"
+import { Quote } from "lucide-react"
+
 
 
 interface AlumniReviewProps {
@@ -10,19 +12,55 @@ interface AlumniReviewProps {
   id: number
 }
 
-export default function AlumniReview({ firstName, lastName, generation, review, id }: AlumniReviewProps) {
+export default function AlumniReview({
+  firstName,
+  lastName,
+  generation,
+  review,
+  id,
+}: AlumniReviewProps) {
   return (
-    <div className={cn(
-    "rounded-tl-[15px] rounded-tr-[15px] rounded-br-[15px] bg-upc-muted overflow-hidden h-[235px] shadow-xl shadow-gray-300",
-    "flex flex-col items-stretch gap-[0.1em] pt-2",
-      )}>
-      <div className="flex items-start">
-        <AlumniTopPart name={firstName} surname={lastName} generation={generation} id={id} />
-      </div>
+    <div
+      className={cn(
+        "relative overflow-hidden",
+        "rounded-tl-[15px] rounded-tr-[15px] rounded-br-[15px]",
+        "bg-upc-muted h-[235px]",
+        "shadow-xl shadow-gray-300",
+        "flex flex-col gap-[0.1em] pt-2",
+      )}
+    >
+      {/* Comillas decorativas */}
+      <Quote
+        aria-hidden
+        className="
+          absolute -top-4 -right-3
+          h-24 w-24
+          text-upc
+          rotate-180
+          pointer-events-none
+          select-none
+        "
+      />
 
-      <div className="flex gap-[1px] px-3 ">
-        <p className="text-[12px] text-gray-600 font-normal leading-normal line-clamp-6">{review}</p>
+
+      {/* Contenido real */}
+      <div className="relative z-10">
+        <div className="flex items-start">
+          <AlumniTopPart
+            name={firstName}
+            surname={lastName}
+            generation={generation}
+            id={id}
+          />
+        </div>
+
+        <div className="px-3">
+          <p className="text-[12px] text-gray-700 font-normal leading-normal line-clamp-6">
+            {review}
+          </p>
+        </div>
       </div>
     </div>
   )
 }
+
