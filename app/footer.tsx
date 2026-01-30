e1242ced241e8fc6577c8d9de8fd7c846f7f738c
Pau Fernández <pau.fernandez@upc.edu>
2026-01-30 19:50:22 +0100

    Ajusto l'amplada del footer, també.

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,8 +4,8 @@ import Logo from "@/assets/logo-web-gced-white.svg"
 
 export default function Footer() {
   return (
-    <footer className="bg-[#007BC0] text-white mt-auto">
-      <div className="px-6 py-6 flex flex-col lg:flex-row items-center justify-between gap-4">
+    <footer className="bg-upc text-white mt-auto">
+      <div className="w-6xl h-[10em] mx-auto px-6 py-6 flex flex-col lg:flex-row items-start justify-between gap-4">
         <Link href="/">
           <Image src={Logo} alt="Logotip Web GCED" width={200} height={50} />
         </Link>
