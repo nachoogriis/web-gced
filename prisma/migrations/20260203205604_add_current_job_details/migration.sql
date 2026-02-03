37f844ea8cc9a3b8030b3a30ec9d8041918ddfee
nachoogriis <nachogrismartin@gmail.com>
2026-02-03 23:42:57 +0100

    Update database fields for statistics and card completeness

---
+++ prisma/migrations/20260203205604_add_current_job_details/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Alumni" ADD COLUMN "currentJobDescription" TEXT;
+ALTER TABLE "Alumni" ADD COLUMN "currentJobKeywordsDomain" TEXT;
+ALTER TABLE "Alumni" ADD COLUMN "currentJobKeywordsSpecialty" TEXT;
+ALTER TABLE "Alumni" ADD COLUMN "currentJobRelatedToGCED" TEXT;
+ALTER TABLE "Alumni" ADD COLUMN "currentOrganization" TEXT;
+ALTER TABLE "Alumni" ADD COLUMN "currentPosition" TEXT;
+ALTER TABLE "Alumni" ADD COLUMN "currentSituation" TEXT;
